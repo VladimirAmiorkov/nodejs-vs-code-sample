5049437d8d828173577241131b8da16a8064499c
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-01-27 16:22:04 +0200

    Changed target to es6 and added a Promise showcase

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ class Sum {
     public a: number;
     public b: number;
 
-    constructor (a: number, b: number) {
+    constructor(a: number, b: number) {
         this.a = a;
         this.b = b;
     }
@@ -14,3 +14,12 @@ class Sum {
 
 var sum = new Sum(3, 2);
 console.log(sum.a + " + " + sum.b + " = " + sum.Calculate());
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        var sum = new Sum(123456789, 987654321);
+        resolve(sum.a + " + " + sum.b + " = " + sum.Calculate())
+    }, 1000);
+})
+    .then(console.log)
+    .catch(console.error)
